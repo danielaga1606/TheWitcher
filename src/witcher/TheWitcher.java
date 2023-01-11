f7e3183af016a6558bdae00e0e3321e3bb8d0879
Daniela <81825319+danielaga1606@users.noreply.github.com>
2023-01-11 12:42:09 +0100

    AñIDOS LOS ATRIBUTOS

--- src/witcher/TheWitcher.java
+++ src/witcher/TheWitcher.java
@@ -1,7 +1,9 @@
 package witcher;
 
 public class TheWitcher {
-
+private String name;
+private int health;
+private boolean hassSould;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
