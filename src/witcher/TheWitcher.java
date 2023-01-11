adcfaf14ee55442d421327ccc5b3e69fb4f50bc7
Daniela <81825319+danielaga1606@users.noreply.github.com>
2023-01-11 12:48:35 +0100

    AñADIDOS LOS GETTERS AND SETTERS

--- src/witcher/TheWitcher.java
+++ src/witcher/TheWitcher.java
@@ -2,6 +2,24 @@ package witcher;
 
 public class TheWitcher {
 private String name;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getHealth() {
+	return health;
+}
+public void setHealth(int health) {
+	this.health = health;
+}
+public boolean isHassSould() {
+	return hassSould;
+}
+public void setHassSould(boolean hassSould) {
+	this.hassSould = hassSould;
+}
 private int health;
 private boolean hassSould;
 	public static void main(String[] args) {
