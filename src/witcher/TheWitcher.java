88fcafd262894506dbac7a1c33918d4aaac3e43a
Daniela <81825319+danielaga1606@users.noreply.github.com>
2023-01-11 13:40:23 +0100

    método isDeath

--- src/witcher/TheWitcher.java
+++ src/witcher/TheWitcher.java
@@ -2,6 +2,9 @@ package witcher;
 
 public class TheWitcher {
 private String name;
+private int health;
+private boolean hasSould;
+private boolean hassSould;
 public String getName() {
 	return name;
 }
@@ -20,12 +23,37 @@ public boolean isHassSould() {
 public void setHassSould(boolean hassSould) {
 	this.hassSould = hassSould;
 }
-private int health;
-private boolean hassSould;
+
+
+
+public boolean isDeath() {
+	if (health==0) {
+		return true;
+	}
+	else if(!hasSould) {
+		return true; 
+	}
+	else {
+		return false;
+	}
+}
+
+
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		TheWitcher wizard = new TheWitcher();
+		wizard.setName("Merlin");
+		wizard.setHealth(15);
+		wizard.setHassSould(true);
+  if (wizard.isDeath()== true) {
+	  System.out.println(wizard.getName()+" está muerto");
+  }
+  else {
+	  System.out.println(wizard.getName()+" está vivo");
+  }
 		
 		
 	}
